7c8b949fa717ebc3e36304977d1e42c81a8b6eed
Jaymie Gobbett <jgobbett@F3C016.42adel.org.au>
2022-05-26 19:36:17 +0930

    almost fin ex01

--- 00/ex01/main.cpp
+++ 00/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jgobbett <jgobbett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/26 13:02:40 by jgobbett          #+#    #+#             */
-/*   Updated: 2022/05/26 16:58:42 by jgobbett         ###   ########.fr       */
+/*   Updated: 2022/05/26 19:36:03 by jgobbett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,61 +35,118 @@ class PhoneBook
 {
 	public:
 	Contact Cons[8];
+	int		num;
 };
+
+void	putst(std::string temp)
+{
+	std::cout << " | ";
+	if (temp.size() > 10)
+	{
+		temp.resize(9);
+		std::cout << temp;
+		std::cout << ".";
+	}
+	else
+		std::cout << temp;
+}
 
 int	main(void)
 {
 	PhoneBook PB;
 	std::string	command;
+	int	selector;
+	PB.num = 0;
 	while (1)
 	{
 		system("clear");
-		std::cout << "command" << std::endl;
+		std::cout << "command - ADD 1, SEARCH 2, EXIT 3" << std::endl;
 		std::cin >> command;
-		if (command == "ADD")
+		if (command == "ADD" || command == "1")
 		{
+			system("clear");
 			std::cout << "ADD New Contact"<< std::endl;
 			for (int i = 1; i >= 0; i--)
 				PB.Cons[i] = PB.Cons[i - 1];
-			std::cout << "name of contact"<< std::endl;
+			std::cout << "first name of contact"<< std::endl;
 			std::cin >> PB.Cons[0].name;
 			while (PB.Cons[0].name.size() < 10)
 				PB.Cons[0].name.append(" ");
+			system("clear");
+
+			std::cout << "last name number of ";
+			std::cout << PB.Cons[0].name << std::endl;
+			std::cin >> PB.Cons[0].Last_name;
+			while (PB.Cons[0].Last_name.size() < 10)
+				PB.Cons[0].Last_name.append(" ");
+			system("clear");
+
+			std::cout << "nick name number of ";
+			std::cout << PB.Cons[0].name << std::endl;
+			std::cin >> PB.Cons[0].Nick_name;
+			while (PB.Cons[0].Nick_name.size() < 10)
+				PB.Cons[0].Nick_name.append(" ");
+			system("clear");
+
 			std::cout << "phone number of ";
 			std::cout << PB.Cons[0].name << std::endl;
 			std::cin >> PB.Cons[0].phone;
 			while (PB.Cons[0].phone.size() < 10)
 				PB.Cons[0].phone.append(" ");
+			system("clear");
+
+			std::cout << "darkest secret of ";
+			std::cout << PB.Cons[0].name << std::endl;
+			std::cin >> PB.Cons[0].secret;
+			while (PB.Cons[0].secret.size() < 10)
+				PB.Cons[0].secret.append(" ");
+			if (PB.num < 8)
+				PB.num++;
 		}
-		else if (command == "SEARCH")
+		else if (command == "SEARCH" || command == "2")
 		{
-			for (int i = 0; i < 8 ; i++)
+			for (int i = 0; i < PB.num; i++)
 			{
 				std::string temp;
 				std::cout << i + 1;
 				std::cout << " | ";
-				if (PB.Cons[i].name.size() > 10)
-				{
-					temp = PB.Cons[i].name;
-					temp.resize(9);
-					std::cout << temp;
-					std::cout << ".";
-				}
-				else
-					std::cout << PB.Cons[i].name;
+				putst(PB.Cons[i].name);
 				std::cout << " | ";
-				if (PB.Cons[i].phone.size() > 10)
-				{
-					temp = PB.Cons[i].phone;
-					temp.resize(9);
-					std::cout << temp;
-					std::cout << "." << std::endl;
-				}
-				else
-					std::cout << PB.Cons[i].phone << std::endl;
+				putst(PB.Cons[i].Last_name);
+				std::cout << " | ";
+				putst(PB.Cons[i].Nick_name);
+				std::cout << " | ";
+				putst(PB.Cons[i].phone);
+				std::cout << " | ";
+				putst(PB.Cons[i].secret);
+				std::cout << " | " << std::endl;
+			}
+			std::cin >> selector;
+			selector--;
+			system("clear");
+			if (selector < PB.num || selector < 0)
+			{
+				std::cout << PB.Cons[selector].name << std::endl;
+				std::cout << PB.Cons[selector].Last_name << std::endl;
+				std::cout << PB.Cons[selector].Nick_name << std::endl;
+				std::cout << PB.Cons[selector].phone << std::endl;
+				std::cout << PB.Cons[selector].secret << std::endl;
+			}
+			else
+			{
+				std::cout << "invalid" << std::endl;
+				command = "B";
+			}
+			while (1)
+			{
+				std::cout << "Type B to go back" << std::endl;
+				if (command == "B")
+					break ;
+				std::cin >> command;
 			}
 		}
-		else if (command == "EXIT")
+		else if (command == "EXIT"  || command == "3")
 			break ;
+		command = "N";
 	}
 }
