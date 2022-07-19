85450aaa75aa77a65b07d06f7b73b1a83bd63334
jaymie gobbett <jjjj7350@gmail.com>
2022-07-19 15:51:31 +0930

    a

--- 06/ex00/main.cpp
+++ 06/ex00/main.cpp
@@ -1,7 +1,27 @@
 #include <string.h>
 #include <iostream>
+#include <sstream>
 using std::string;
 using std::cout;
 using std::endl;
 
-int main()
+int main(int ac, string av)
+{
+	char	c;
+	int		i;
+	float	f;
+	double	d;
+
+	i = stoi(av);
+	c = i;
+	f = stof(av);
+	d = stod(av);
+
+	if (isprint(c))
+		cout << "char: " << c << endl;
+	else
+		cout << "char: " << "Non displayable" << endl;
+	cout << "int: " << i << endl;
+	cout << "float: " << f << endl;
+	cout << "double: " << d << endl;
+}
